9f9adeda16362f81927ab595bb1d8b246acfbc37
tobozo <tobozo@users.noreply.github.com>
2022-03-21 17:08:36 +0100

    Stronger package version check

--- config_check.h
+++ config_check.h
@@ -31,8 +31,34 @@
 #if !defined ESP_ARDUINO_VERSION_VAL
   #error "Invalid esp32-core version (expected: >=2.0.2)"
 #else
+  //#if __has_include("core_version.h") // for platformio
+  #include "core_version.h"
+  //#endif
   #if defined ESP_ARDUINO_VERSION && ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(2, 0, 0)
-    // fine, that's our target esp32 core
+    #if ARDUINO_ESP32_GIT_VER == 0x44c11981
+      // Missing SD.open(create-subdirs), SD.readRAW(), SD.writeRAW(), SD.numSectors() and SD.cardSize()
+      #error "ESP32 Arduino 2.0.0 (0x44c11981) is unsupported (>=2.0.2 required)"
+    #elif ARDUINO_ESP32_GIT_VER == 0x15bbd0a1
+      // This version introduces SD.open(create-subdirs)
+      // Missing SD.readRAW() / SD.writeRAW() / SD.numSectors() and SD.cardSize()
+      #error "ESP32 Arduino 2.0.1 RC1 (0x15bbd0a1) is unsupported (>=2.0.2 required)"
+    #elif ARDUINO_ESP32_GIT_VER == 0xd218e58f
+      // OTA support broken, missing SD.readRAW() / SD.writeRAW() / SD.numSectors() and SD.cardSize()
+      #error "ESP32 Arduino 2.0.1 (0xd218e58f) is unsupported (>=2.0.2 required)"
+    #elif ARDUINO_ESP32_GIT_VER == 0xcaef4006
+      // Fine, that's the exact esp32 core version this app is expecting.
+      // This version introduces the much needed SD.readRAW(), SD.writeRAW(), SD.numSectors() and SD.cardSize()
+      // However SD support needs patching and is highly unstable in some situations.
+      #warning "ESP32 Arduino 2.0.2 (0xcaef4006) support it still experimental, and SD library needs a patch"
+    #else
+      #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(2, 0, 2)
+        // 2.0.3 and further versions
+        #warning "Untested arduino-esp32 version, feel free to experiment"
+      #else
+        // ESP32 Arduino 1.0.6?
+        #error "Unknown arduino-esp32 version, comment this out if your package version >= 2.0.2"
+      #endif
+    #endif
   #else
     #error "Invalid esp32-core version (expected: >=2.0.2)"
   #endif
